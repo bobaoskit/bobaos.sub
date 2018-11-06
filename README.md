10d75a9647adcd74e6fb04fea66cda58173f505d
Vladimir Shabunin <va.shabunin@physics.msu.ru>
2018-11-06 20:01:44 +0300

    hello, friend

---
+++ README.md
@@ -0,0 +1,81 @@
+# bobaos.sub - client lib for bobaos.pub
+ 
+ ## Installation
+ 
+ ```
+ npm install --save bobaos.sub
+ ```
+ 
+ ## Usage example
+ 
+ Source:
+ ```js
+ const BobaosSub = require('bobaos.sub');
+ 
+ let my = BobaosSub();
+ 
+ my.on("connect", _ => {
+   console.log("connected to ipc, still not subscribed to channels");
+ });
+ 
+ my.on("ready", async _ => {
+   try {
+     console.log("hello, friend");
+     console.log("ping:", await my.ping());
+     console.log("get sdk state:", await my.getSdkState());
+     console.log("get value:", await my.getValue([1, 107, 106]));
+     console.log("get stored value:", await my.getValue([1, 107, 106]));
+     console.log("get server item:", await my.getServerItem("SerialNumber"));
+     console.log("set value:", await my.setValue({id: 103, value: 0}));
+     console.log("read value:", await my.readValue([1, 103, 104, 105]));
+     console.log("get programming mode:", await my.getProgrammingMode());
+     console.log("set programming mode:", await my.setProgrammingMode(true));
+     console.log("get parameter byte", await my.getParameterByte([1, 2, 3, 4]));
+     console.log("reset", await my.reset());
+   } catch(e) {
+     console.log("err", e.message);
+   }
+ });
+ 
+ my.on("datapoint value", payload => {
+   console.log("broadcasted datapoint value: ", payload);
+ });
+ 
+ my.on("server item", payload => {
+   console.log("broadcasted server item: ", payload);
+ });
+ 
+ my.on("sdk state", payload => {
+   console.log("broadcasted sdk state: ", payload);
+ });
+ 
+ ```
+ 
+ Output:
+ ```
+ hello, friend
+ ping: true
+ get sdk state: ready
+ get value: [ { id: 1, value: 20.7, raw: [ 12, 11 ] },
+   { id: 107, value: false, raw: [ 0 ] },
+   { id: 106, value: false, raw: [ 0 ] } ]
+ get stored value: [ { id: 1, value: 20.7, raw: [ 12, 11 ] },
+   { id: 107, value: false, raw: [ 0 ] },
+   { id: 106, value: false, raw: [ 0 ] } ]
+ get server item: { id: 8,
+   name: 'SerialNumber',
+   value: [ 0, 197, 1, 1, 118, 183 ] }
+ set value: { id: 103, value: false, raw: [ 0 ] }
+ broadcasted datapoint value:  { id: 103, value: false, raw: [ 0 ] }
+ read value: null
+ get programming mode: true
+ broadcasted datapoint value:  { id: 1, value: 20.6, raw: [ 12, 6 ] }
+ broadcasted datapoint value:  { id: 103, value: false, raw: [ 0 ] }
+ set programming mode: { id: 15, name: 'ProgrammingMode', value: true }
+ broadcasted datapoint value:  { id: 105, value: false, raw: [ 0 ] }
+ get parameter byte [ 1, 3, 5, 7 ]
+ broadcasted sdk state:  stop
+ broadcasted sdk state:  ready
+ reset null
+ broadcasted datapoint value:  { id: 1, value: 20.6, raw: [ 12, 6 ] }
+ ```
---
+++ index.js
@@ -0,0 +1,142 @@
+const EE = require("events");
+const Redis = require("redis");
+const Queue = require("bee-queue");
+
+let BobaosBQ = params => {
+  let _params = {
+    redis: null,
+    request_channel: "bobaos_req",
+    broadcast_channel: "bobaos_bcast"
+  };
+
+  let self = new EE();
+  Object.assign(_params, params);
+  const jqueue = new Queue(_params.request_channel, {
+    redis: _params.redis,
+    isWorker: false
+  });
+
+  let jobs = [];
+
+  jqueue.on("ready", _ => {
+    self.emit("ready");
+  });
+
+  jqueue.on("job succeeded", (id, result) => {
+    // hack: sometimes this event is fired before job.save.then
+    // so, set timeout to be sure that job save then was called
+    setTimeout(_ => {
+      const findById = t => t.id === id;
+      let found = jobs.findIndex(findById);
+      if (found > -1) {
+        // TODO: resolve/reject
+        let {method, payload} = result;
+        if (method === "success") {
+          jobs[found].callback(null, payload);
+        }
+        if (method === "error") {
+          jobs[found].callback(new Error(payload));
+        }
+        jobs.splice(found, 1);
+      } else {
+        // there will be jobs from other clients, so do nothing.
+        // console.log(`Couldn't find job with id ${id}`);
+      }
+    }, 1);
+  });
+
+  // Never used?
+  jqueue.on("job failed", (id, result) => {
+    console.log(`Job ${id} failed with result: ${result}`);
+  });
+
+  self.commonRequest = (method, payload) => {
+    return new Promise((resolve, reject) => {
+      jqueue
+        .createJob({ method: method, payload: payload })
+        .save()
+        .then(job => {
+          let { id } = job;
+          let callback = (err, result) => {
+            if (err) {
+              return reject(err);
+            }
+
+            resolve(result);
+          };
+          jobs.push({ id: id, callback: callback });
+        })
+        .catch(e => {
+          reject(e);
+        });
+    });
+  };
+
+  // service
+  self.ping = _ => {
+    return self.commonRequest("ping", null);
+  };
+  self.getSdkState = _ => {
+    return self.commonRequest("get sdk state", null);
+  };
+  self.reset = _ => {
+    return self.commonRequest("reset", null);
+  };
+
+  // datapoints
+  self.getDescription = payload => {
+    return self.commonRequest("get description", payload);
+  };
+  self.getValue = payload => {
+    return self.commonRequest("get value", payload);
+  };
+  self.getStoredValue = payload => {
+    return self.commonRequest("get stored value", payload);
+  };
+  self.setValue = payload => {
+    return self.commonRequest("set value", payload);
+  };
+  self.readValue = payload => {
+    return self.commonRequest("read value", payload);
+  };
+  self.getServerItem = payload => {
+    return self.commonRequest("get server item", payload);
+  };
+  self.setProgrammingMode = payload => {
+    return self.commonRequest("set programming mode", payload);
+  };
+  self.getProgrammingMode = _ => {
+    return self.commonRequest("get programming mode", null);
+  };
+  self.getParameterByte = payload => {
+    return self.commonRequest("get parameter byte", payload);
+  };
+  self.pollValues = _ => {
+    return self.commonRequest("poll values", null);
+  };
+
+  // now events
+  const redisSub = Redis.createClient(_params.redis);
+  redisSub.on("message", function(channel, message) {
+    try {
+      let { method, payload } = JSON.parse(message);
+      if (method === "datapoint value") {
+        return self.emit("datapoint value", payload);
+      }
+      if (method === "server item") {
+        return self.emit("server item", payload);
+      }
+      if (method === "sdk state") {
+        return self.emit("sdk state", payload);
+      }
+    } catch (e) {
+      console.log(`Error processing broadcast message: ${e.message}`);
+    }
+  });
+
+  redisSub.subscribe(_params.broadcast_channel);
+  return self;
+};
+
+module.exports = BobaosBQ;
+
